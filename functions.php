3556de7342b3e3d332b2718ef9b8861c6a92d706
Andrianina24 <rakotozananya5@gmail.com>
2024-03-19 15:19:40 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,13 +2,13 @@
 include ('./inc/connect.inc.php');
 
 function getProducts($con){
-    $sql= "SELECT distinct(item) from Products";
+    $sql= "SELECT distinct(item) from products";
     $result= mysqli_query($con,$sql);
     return $result;
 }
 
 function getProductByItem($con,$item){
-    $req= "SELECT * from Products where item='%s'";
+    $req= "SELECT * from products where item='%s'";
     $sql=sprintf($req,$item);
     $result= mysqli_query($con,$sql);
     return $result;
